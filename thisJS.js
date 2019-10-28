ec3bb222c724247b1ae25aff1c7a7ee055eb4a09
mercyu <55250413+mercyu@users.noreply.github.com>
2019-10-28 23:54:58 +0300

    Update thisJS.js

--- thisJS.js
+++ thisJS.js
@@ -2,14 +2,17 @@ function updatePrice() {
     let s = document.getElementsByName("types");
     let select = s[0];
     let price = 0;
+    let currPrice1=0;
+    let currPrice2=0;
     let prices = getPrices();
     let priceIndex = parseInt(select.value) - 1;
     if (priceIndex >= 0) {
       price = prices.types[priceIndex];
     }
+    // Скрываем или показываем радиокнопки
     let radioDiv = document.getElementById("radios");
     radioDiv.hidden = (select.value == "2" ? false : true);
-
+    // Смотрим, какая опция выбрана
     let radios = document.getElementsByName("options");
     radios.forEach(function(radio) {
       if (radio.checked) {
@@ -19,21 +22,24 @@ function updatePrice() {
         }
       }                                  
     });
-
+     // Скрываем или показываем чекбокс
     let checkDiv = document.getElementById("checkbox");
   checkDiv.hidden = (select.value == "3" ? false : true);
-
+     // Проверяем, выполнено ли свойство
     if (checkbox.checked) {
       let propPrice = prices.property.prop;
       if (propPrice !== undefined) {
         price += propPrice;
       }
     }
-  let currentCount=+document.getElementById("count").value;
-  price*=currentCount;
+  let Counter=+document.getElementById("count").value;
+  if(Counter!==undefined)
+  price*=Counter;
+  Counter=0;
   let totalPrice = document.getElementById("totalPrice");
   totalPrice.innerHTML = price + " рублей";
 }
+     // Данные о товарах
 function getPrices() {
     return {
       types: [200, 600, 800],
@@ -58,40 +64,32 @@ function getPrices() {
     let select = s[0];
     // Назначаем обработчик на изменение select.
     select.addEventListener("change", function(event) {
-      let target = event.target;
-      console.log(target.value);
-      count.innerHTML=0;
-   
+        let target = event.target;
+        console.log(target.value);
+        updatePrice();
+      });
+      
+      // Назначаем обработчик радиокнопок.  
+      let radios = document.getElementsByName("options");
+      radios.forEach(function(radio) {
+        radio.addEventListener("change", function(event) {
+          let r = event.target;
+          console.log(r.value);
+          updatePrice();
+        });
+      });
     
-    // Назначаем обработчик радиокнопок.  
-    let radios = document.getElementsByName("options");
-    radios.forEach(function(radio) {
-      radio.addEventListener("change", function(event) {
-        let r = event.target;
-        console.log(r.value);
-        updatePrice();
-        let counter=document.getElementById("count");
-counter.addEventListener("input",()=>{
-updatePrice();
-});
-
+        // Назначаем обработчик чекбоксу.  
+      let checkboxes = document.querySelectorAll("#checkbox input");
+      checkboxes.forEach(function(checkbox) {
+        checkbox.addEventListener("change", function(event) {
+          let c = event.target;
+          console.log(c.name);
+          console.log(c.value);
+          updatePrice();
+        });
       });
+    
+      updatePrice();
     });
-  
-      // Назначаем обработчик чекбокса.  
-    //let checkboxes = document.getElementsByName("prop");
-      checkbox.addEventListener("change", function(event) {
-        let c = event.target;
-        console.log(c.name);
-        console.log(c.value);
-        updatePrice();
-        let counter=document.getElementById("count");
-counter.addEventListener("input",()=>{
-updatePrice();
-});
-      });
-
-// Назначаем обработчик поля ввода.
-    updatePrice();
-  });
-   });
+    
